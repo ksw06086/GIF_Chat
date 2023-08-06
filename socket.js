e7b3beb6bab7eeed91e1944faccf221d6ac6a11c
unknown <k-sunwo@naver.com>
2023-08-06 20:20:56 +0900

    gif_chat : Socket.IO 모듈 사용하기

--- socket.js
+++ socket.js
@@ -1,31 +1,30 @@
-const WebSocket = require('ws');
+const SocketIO = require('socket.io');
 
 // 서버
 module.exports = (server) => {
     // 웹소켓과 익스프레스 서버가 연결됨
-    const wss = new WebSocket.Server({ server });
+    const io = SocketIO(server, { path: '/socket.io' });
 
     // 웹 소켓 연결 맺을 때 처음 한번은 클라이언트에서 연결을 맺어주어야 함
-    wss.on('connection', (ws, req) => {
+    io.on('connection', (socket) => {
+        const req = socket.request;
         // 클라이언트의 ip주소 가져오기
         const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-        console.log('새로운 클라이언트 접속', ip);
-        ws.on('message', (message) => {
-            console.log(message.toString());
-        })
-        ws.on('error', console.error);
-        ws.on('close', () => { // 연결이 끊어졌으면 3초마다 보내는 것 중단함
-            console.log('클라이언트 접속 해제', ip);
-            clearInterval(ws.interval); 
-        })
+        console.log('새로운 클라이언트 접속', ip, socket.id, req.ip); // socket.id 특정인과 연결, 연결끊기 가능(연결에 id가 붙여있는 느낌)
 
+        // 소켓 해제
+        socket.on('disconnect', () => {
+            console.log('클라이언트 접속 해제', ip, socket.id);
+            clearInterval(socket.interval); 
+        })
+        // 응답 받기 
+        socket.on('reply', (data) => {
+            console.log(data);
+        })
+        socket.on('error', console.error);
         // 3초마다 클라이언트로 실시간 전송함        
-        ws.interval = setInterval(() => {
-            // readyState가 오픈상태일 때(처음 맺고 보냈을 때 안 열린 상황일수도 있다.)
-            // readyState 상태 : OPEN, CONNECTING, CLOSING, CLOSED 4가지 있음, OPEN 제외 상태에서는 통신불가
-            if(ws.readyState === ws.OPEN){
-                ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
-            }
+        socket.interval = setInterval(() => {
+            socket.emit('news', 'Hello Socket.IO');
         }, 3000);
     });
 }
---
+++ socket.ws.js
@@ -0,0 +1,32 @@
+// ws(웹소켓)은 간단한 채팅 구현 때 사용함 대부분 Socket.io로 구현
+const WebSocket = require('ws');
+
+// 서버
+module.exports = (server) => {
+    // 웹소켓과 익스프레스 서버가 연결됨
+    const wss = new WebSocket.Server({ server });
+
+    // 웹 소켓 연결 맺을 때 처음 한번은 클라이언트에서 연결을 맺어주어야 함
+    wss.on('connection', (ws, req) => {
+        // 클라이언트의 ip주소 가져오기
+        const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        console.log('새로운 클라이언트 접속', ip);
+        ws.on('message', (message) => {
+            console.log(message.toString());
+        })
+        ws.on('error', console.error);
+        ws.on('close', () => { // 연결이 끊어졌으면 3초마다 보내는 것 중단함
+            console.log('클라이언트 접속 해제', ip);
+            clearInterval(ws.interval); 
+        })
+
+        // 3초마다 클라이언트로 실시간 전송함        
+        ws.interval = setInterval(() => {
+            // readyState가 오픈상태일 때(처음 맺고 보냈을 때 안 열린 상황일수도 있다.)
+            // readyState 상태 : OPEN, CONNECTING, CLOSING, CLOSED 4가지 있음, OPEN 제외 상태에서는 통신불가
+            if(ws.readyState === ws.OPEN){
+                ws.send('서버에서 클라이언트로 메시지를 보냅니다.');
+            }
+        }, 3000);
+    });
+}
